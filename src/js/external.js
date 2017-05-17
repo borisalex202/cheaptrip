0fce70635cd5674ae837f81c19c9841fe833e2cb
index1597 <aleksandrboris@MacBook-Pro-Aleksandr.local>
2017-05-17 21:09:18 +0300

    Adding 'toastr' plugin and initializing him

--- src/js/external.js
+++ src/js/external.js
@@ -3,3 +3,4 @@
 @@include('../../bower_components/air-datepicker/dist/js/datepicker.min.js');
 @@include('../../bower_components/slick-carousel/slick/slick.min.js');
 @@include('../../bower_components/dropzone/dist/dropzone.js');
+@@include('../../bower_components/toastr/toastr.min.js');
